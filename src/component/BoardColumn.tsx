61ed93680322a14fb265f1393dc3f5df16caf05d
shm <malekishayan79@gmail.com>
2025-10-01 00:32:45 +0330

    feat(memo): use useMemo for task filtering

--- src/component/BoardColumn.tsx
+++ src/component/BoardColumn.tsx
@@ -5,7 +5,7 @@ import { createPortal } from "react-dom";
 import { useQuery } from "@tanstack/react-query";
 import client from "../client";
 import { Task } from "../interfaces/Task.interface";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import FilterContext from "../context/filterCotext";
 
 interface ColumnProps {
@@ -14,46 +14,68 @@ interface ColumnProps {
   setIsNewTaskModalOpen: (isNewTaskModalOpen: boolean) => void;
 }
 
-function BoardColumn({ columnId, isNewTaskModalOpen, setIsNewTaskModalOpen }: ColumnProps) {  
-  const { FilterState: { priority, storyPoint, titleSearch } } = useContext(FilterContext);
-  const { data: tasks , isLoading, isError } = useQuery({
+function BoardColumn({ columnId, isNewTaskModalOpen, setIsNewTaskModalOpen }: ColumnProps) {
+  const {
+    FilterState: { priority, storyPoint, titleSearch },
+  } = useContext(FilterContext);
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["tasks"],
     queryFn: () => client.get("tasks").then((res) => res.data),
   });
-  
-  const { data: column, isLoading: isColumnLoading, isError: isColumnError } = useQuery({
+
+  const {
+    data: column,
+    isLoading: isColumnLoading,
+    isError: isColumnError,
+  } = useQuery({
     queryKey: ["columns", columnId],
     queryFn: () => client.get(`columns/${columnId}`).then((res) => res.data),
   });
 
-  
-  // @todo: fix loading and error handling 
-  if (isLoading || isColumnLoading) return <Spinner />
-  if (isError || isColumnError) return <Text color="red.500">Error loading tasks</Text>
+  // @todo: fix loading and error handling
+  if (isLoading || isColumnLoading) return <Spinner />;
+  if (isError || isColumnError) return <Text color="red.500">Error loading tasks</Text>;
 
-  let tasksInColumn = tasks?.filter((task: Task) => task.columnId === +columnId);
-  if (priority !== "All") tasksInColumn = tasksInColumn?.filter((task: Task) => task.priority === priority);
-  if (storyPoint) tasksInColumn = tasksInColumn?.filter((task: Task) => task.storyPoints === storyPoint);
-  if (titleSearch) tasksInColumn = tasksInColumn?.filter((task: Task) => task.title.toLowerCase().includes(titleSearch.toLowerCase()));
+  const tasksInColumn = useMemo(() => {
+    let tasksInColumn = tasks?.filter((task: Task) => task.columnId === +columnId);
+    if (priority !== "All")
+      tasksInColumn = tasksInColumn?.filter((task: Task) => task.priority === priority);
+    if (storyPoint)
+      tasksInColumn = tasksInColumn?.filter((task: Task) => task.storyPoints === storyPoint);
+    if (titleSearch)
+      tasksInColumn = tasksInColumn?.filter((task: Task) =>
+        task.title.toLowerCase().includes(titleSearch.toLowerCase())
+      );
+    return tasksInColumn;
+  }, [tasks, priority, storyPoint, titleSearch]);
 
   return (
     <>
-    <VStack>
-      <Card.Root w="100%" h="100%">
-        <Card.Header>
-          <Box display="flex" borderBottom="1px solid gray" pb={2}>
-          <Text fontSize="lg" fontWeight="bold" mr={4}>
-            {column.title}
-          </Text>
-          <Badge size="sm" bgColor='gray.500' color='white'>{tasksInColumn.length} tasks</Badge>
-          </Box>
-        </Card.Header>
-        {tasksInColumn.map(({id: taskId}: Task) => (
-          <TaskCard key={taskId} taskId={taskId}/>
-        ))}
-      </Card.Root>
-    </VStack>
-    {createPortal(<NewTaskModal isNewTaskModalOpen={isNewTaskModalOpen} onClose={setIsNewTaskModalOpen} />, document.getElementById("modal-root") as HTMLElement)}
+      <VStack>
+        <Card.Root w="100%" h="100%">
+          <Card.Header>
+            <Box display="flex" borderBottom="1px solid gray" pb={2}>
+              <Text fontSize="lg" fontWeight="bold" mr={4}>
+                {column.title}
+              </Text>
+              <Badge size="sm" bgColor="gray.500" color="white">
+                {tasksInColumn.length} tasks
+              </Badge>
+            </Box>
+          </Card.Header>
+          {tasksInColumn.map(({ id: taskId }: Task) => (
+            <TaskCard key={taskId} taskId={taskId} />
+          ))}
+        </Card.Root>
+      </VStack>
+      {createPortal(
+        <NewTaskModal isNewTaskModalOpen={isNewTaskModalOpen} onClose={setIsNewTaskModalOpen} />,
+        document.getElementById("modal-root") as HTMLElement
+      )}
     </>
   );
 }
